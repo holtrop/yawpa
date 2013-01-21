1a1b18b62eecae6898faad4cfb0845abb2c50442
Josh Holtrop <jholtrop@gmail.com>
2013-01-20 20:35:34 -0500

    reformat spec to test parse() results separately

--- spec/yawpa_spec.rb
+++ spec/yawpa_spec.rb
@@ -5,7 +5,9 @@ describe Yawpa do
     it "returns everything as arguments when no options present" do
       config = { }
       params = ['one', 'two', 'three', 'four']
-      Yawpa.parse(config, params).should eq([[], params])
+      opts, args = Yawpa.parse(config, params)
+      opts.should eq([])
+      args.should eq(params)
     end
   end
 end
